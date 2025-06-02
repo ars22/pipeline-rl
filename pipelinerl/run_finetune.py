d09faf341321b3fc2cc61c3b2019b7d0ddf49491
rizar <dimabv@tut.by>
2025-06-02 13:45:16 +0000

    refactor

--- pipelinerl/run_finetune.py
+++ pipelinerl/run_finetune.py
@@ -731,14 +731,9 @@ def rl_finetuning_worker(
                 model.set_gradient_accumulation_boundary(True)
                 model.step()
                 grad_norm = model.get_global_grad_norm() if hasattr(model, "get_global_grad_norm") else None
-                max_grad_norm = model.gradient_clipping() if hasattr(model, "gradient_clipping") else None
                 if isinstance(training_metrics.grad_norm, torch.Tensor):
                     grad_norm = grad_norm.item()
-                if grad_norm is not None:
-                    training_metrics.grad_norm = grad_norm
-                else:
-                    # max_grad_norm and grad_norm are not available 
-                    training_metrics.grad_norm = -1.0
+                training_metrics.grad_norm = grad_norm if grad_norm is not None else -1.0
             else:
                 max_grad_norm = args.get("gradient_clipping_threshold", None)
                 training_metrics.grad_norm = get_accelerator().clip_grad_norm_(model.parameters(), max_grad_norm)
