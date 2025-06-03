29d45d430fabd758abf39fa5f9f59283e4dde76b
Dzmitry Bahdanau <dzmitry.bahdanau@servicenow.com>
2025-06-03 09:46:47 -0400

    simplify

--- pipelinerl/run_preprocess.py
+++ pipelinerl/run_preprocess.py
@@ -128,9 +128,7 @@ def preprocess_dataset(
     rl_config: RLConfig,
 ) -> Dataset:
     preprocess = partial(preprocess_fn, seq_length=seq_length, tokenizer=tokenizer, is_rl=True)
-    columns = ["input_ids", "labels", "attention_mask"] + RL_DATA_COLUMNS
-    if rl_config.filter_zero_advantage_groups and "group_id" not in columns:
-        columns.append("group_id")
+    columns = ["input_ids", "labels", "attention_mask", "group_id"] + RL_DATA_COLUMNS
     
     logger.debug(f"Instantiated preprocess function hash {Hasher.hash(preprocess)}")
 
