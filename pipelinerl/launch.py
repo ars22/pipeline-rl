25c339d8023386aa548b9825ad2e67d52afa3cd5
Nicolas Gontier <gontier.nicolas@yahoo.fr>
2025-06-11 21:20:31 +0000

    hardcode tapeagent in pythonpath

--- pipelinerl/launch.py
+++ pipelinerl/launch.py
@@ -19,7 +19,7 @@ from pipelinerl.world import Job, WorldMap
 logger = logging.getLogger(__name__)
 
 # All the launch commands in this file pass the environment to child processes
-os.environ["PYTHONPATH"] = f"{os.getcwd()}"
+os.environ["PYTHONPATH"] = f"/home/toolkit/TapeAgents"
 os.environ["NCCL_CUMEM_ENABLE"] = "0"
 os.environ["TORCH_DISABLE_SHARE_RDZV_TCP_STORE"] = "1"
 os.environ["HF_DATASETS_DISABLE_PROGRESS_BARS"] = "1"
