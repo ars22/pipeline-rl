fe1f340c0ad22f7a75b5d5039a373f7955c24b1f
violetxi <ziyxiang@stanford.edu>
2025-11-22 23:18:26 +0000

    try 0 entropy

---
+++ recipes/run_omni_entropy-0.001.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python -m pipelinerl.launch \
+--config-name=omni_genrm \
+finetune.rl.entropy_bonus=0.001 \
+output_dir=results/smooth_qwen3-4b_e1e-3 \
+wandb.wandb_project_name=omni-cohen-genrm
