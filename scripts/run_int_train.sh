9637d86bbc3de34c22c65db91d40d976cb85b7ca
violetxi <ziyxiang@stanford.edu>
2026-02-13 00:04:09 -0800

    minor

--- scripts/run_int_train.sh
+++ scripts/run_int_train.sh
@@ -3,4 +3,4 @@
 python -m pipelinerl.launch \
 --config-name=int_train \
 wandb.wandb_project_name=prl_int_train_qwen4b_instruct \
-output_dir=/hai/scratch/ziyxiang/pipeline-rl/results/prl-int-train-16k-8a8f-grpo-n4-d
+output_dir=./results/prl-int-train-16k-8a8f-grpo-n4-d
--- scripts/run_pope_hard_w_gemini_sol.sh
+++ scripts/run_pope_hard_w_gemini_sol.sh
@@ -3,4 +3,4 @@
 python -m pipelinerl.launch \
 --config-name=pope_hard_w_gemin_sol \
 wandb.wandb_project_name=prl_pope_hard_w_gemini_sol_qwen4b_instruct \
-output_dir=/hai/scratch/ziyxiang/pipeline-rl/results/prl-pope-hard-w-gemini-sol-16k-8a8f-grpo-n4-d
+output_dir=./results/prl-pope-hard-w-gemini-sol-16k-8a8f-grpo-n4-d
