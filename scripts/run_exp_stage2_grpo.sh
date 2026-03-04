2696eb7ab8c6f7df0d1b059982646999e5f29e10
violetxi <ziyxiang@stanford.edu>
2026-03-04 00:48:58 -0800

    minor

--- scripts/run_exp_stage2_grpo.sh
+++ scripts/run_exp_stage2_grpo.sh
@@ -2,5 +2,5 @@
 
 python -m pipelinerl.launch \
 --config-name=exp_stage2_grpo \
-wandb.wandb_project_name=prl_ppexp_stage2_qwen4b_instruct \
+wandb.wandb_project_name=prl_exp_stage2_qwen4b_instruct \
 output_dir=./results/prl-exp-stage2-sft-16k
