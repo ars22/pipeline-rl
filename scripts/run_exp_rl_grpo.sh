53e029176b995cb4ce0402df5dc648fb2d7fa5fd
violetxi <ziyxiang@stanford.edu>
2026-03-04 00:18:57 -0800

    single node stage 2

--- scripts/run_exp_rl_grpo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python -m pipelinerl.launch \
---config-name=exp_rl_grpo \
-wandb.wandb_project_name=prl_exp_rl_qwen4b_thinking \
-output_dir=./results/prl-exp-rl-grpo-32k-16a-grpo
---
+++ scripts/run_exp_stage2_sft.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python -m pipelinerl.launch \
+--config-name=exp_stage2_sft \
+wandb.wandb_project_name=prl_ppexp_stage2_qwen4b_instruct \
+output_dir=./results/prl-exp-stage2-sft-16k
