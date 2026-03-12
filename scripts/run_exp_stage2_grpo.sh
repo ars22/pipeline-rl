d29cba9cfa4b15507df4d320d1275efb176131f2
violetxi <ziyxiang@stanford.edu>
2026-03-12 20:39:28 +0000

    minor

--- scripts/run_exp_stage2_grpo.sh
+++ scripts/run_exp_stage2_grpo.sh
@@ -3,4 +3,4 @@
 python -m pipelinerl.launch \
 --config-name=exp_stage2_grpo \
 wandb.wandb_project_name=prl_exp_stage2_qwen4b_instruct \
-output_dir=./results/prl-exp-stage2-sft-16k
+output_dir=./results/prl-exp-stage2-grpo-16k
